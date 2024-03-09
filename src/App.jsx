31215adee1a41a13a32b0d85eb1af0bf6ee75cd1
Shahria Kabir <shahriakabir@Kabirs-Mac.local>
2024-03-09 21:12:05 +0600

    Support

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Hero from "./components/pages/Hero";
+
 import Navbar from "./components/pages/Navbar";
+import Support from "./components/pages/Support";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Support></Support>
     </>
   );
 }
---
+++ src/components/pages/Support.jsx
@@ -0,0 +1,108 @@
+import "./Support.css";
+
+function Support() {
+  return (
+    <>
+      <div>
+        <div className="support-title">
+          <h4>Choose Your Support Tag</h4>
+          <button>RISE A TICKET</button>
+        </div>
+
+        <div className="card-supportContainer">
+          <div className="card-supportbody">
+            <div>
+              <i className="fa-solid fa-house home"></i>
+            </div>
+            <h3>Account Overview</h3>
+
+            <ul>
+              <li>First time, what do I do next?</li>
+              <li>Changing you profile picture and other information</li>
+              <li>I didnt get a confirmation email, what should I do next?</li>
+              <li>
+                What is the refund policy if I have to cancel during the month.
+              </li>
+            </ul>
+          </div>
+          <div className="card-supportbody">
+            <div>
+              <i className="fa-solid fa-closed-captioning subscription"></i>
+            </div>
+            <h3>Subscription Plans</h3>
+
+            <ul>
+              <li>First time, what do I do next?</li>
+              <li>Changing you profile picture and other information</li>
+              <li>I didnt get a confirmation email, what should I do next?</li>
+              <li>
+                What is the refund policy if I have to cancel during the month.
+              </li>
+            </ul>
+          </div>
+          <div className="card-supportbody">
+            <div>
+              <i className="fa-brands fa-paypal payment"></i>
+            </div>
+            <h3>Payment Options</h3>
+
+            <ul>
+              <li>First time, what do I do next?</li>
+              <li>Changing you profile picture and other information</li>
+              <li>I didnt get a confirmation email, what should I do next?</li>
+              <li>
+                What is the refund policy if I have to cancel during the month.
+              </li>
+            </ul>
+          </div>
+          <div className="card-supportbody">
+            <div>
+              <i className="fa-solid fa-bell notification"></i>
+            </div>
+            <h3>Notification Settings</h3>
+
+            <ul>
+              <li>First time, what do I do next?</li>
+              <li>Changing you profile picture and other information</li>
+              <li>I didnt get a confirmation email, what should I do next?</li>
+              <li>
+                What is the refund policy if I have to cancel during the month.
+              </li>
+            </ul>
+          </div>
+          <div className="card-supportbody">
+            <div>
+              <i className="fa-solid fa-user profile"></i>
+            </div>
+            <h3>Profile Preferences</h3>
+
+            <ul>
+              <li>First time, what do I do next?</li>
+              <li>Changing you profile picture and other information</li>
+              <li>I didnt get a confirmation email, what should I do next?</li>
+              <li>
+                What is the refund policy if I have to cancel during the month.
+              </li>
+            </ul>
+          </div>
+          <div className="card-supportbody">
+            <div>
+              <i className="fa-solid fa-lock privacy"></i>
+            </div>
+            <h3>Privacy and Cookies</h3>
+
+            <ul>
+              <li>First time, what do I do next?</li>
+              <li>Changing you profile picture and other information</li>
+              <li>I didnt get a confirmation email, what should I do next?</li>
+              <li>
+                What is the refund policy if I have to cancel during the month.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Support;
